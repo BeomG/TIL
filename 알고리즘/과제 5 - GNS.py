53fdf3fb1172182f39608bede055f9393d2cc1bf
BeomG <tlfem3315@naver.com>
2023-02-08 17:56:28 +0900

    02_08

--- 알고리즘/과제 5 - GNS.py
+++ 알고리즘/과제 5 - GNS.py
@@ -3,5 +3,15 @@ T = int(input())
 for tc in range(1, T+1):
     N, M = input(). split()
     words = list(input().split())
-    lst = [0] * 10
-    
+    lst = [[]for _ in range(10)]
+    for word in words:
+        for i in range(10):
+            if numbers[i] == word:
+                lst[i].append(numbers[i])
+
+    print(f'#{tc}')
+    for i in lst:
+        ans = ' '.join(i)
+        print(ans, end=' ')
+
+
